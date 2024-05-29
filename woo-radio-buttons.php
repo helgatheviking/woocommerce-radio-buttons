144803ffc4ac91baccdfebd8f6f534939ecfd5eb
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-05-29 12:00:21 -0400

    add accessed directly check

--- woo-radio-buttons.php
+++ woo-radio-buttons.php
@@ -23,6 +23,9 @@ function wooradio_plugin_path() {
   // gets the absolute path to this plugin directory 
   return untrailingslashit( plugin_dir_path( __FILE__ ) ); 
 } 
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly.
+}
 
 /**
  * Register scripts
